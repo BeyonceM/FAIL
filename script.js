c768be4d41cf5fcd12f71a6b9b22faa27ff6fd83
BeyonceM <43378793+BeyonceM@users.noreply.github.com>
2019-02-11 10:20:11 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $("#homework").click(function() {
     $("#crumble").toggle();
+  
+    
+});
+$("#crumble").click(function() {
+   $("#throw").toggle();
+});
+
+$("#trash").click(function() {
+   $("#pilles").toggle();
+});
+
+$("#f").click(function() {
+   $("#pilles").toggle();
 });
